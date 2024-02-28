c3eb775b165d438c672754def54f82d216d62f17
Julie Luangpraxay <130415142+julieluangpraxay@users.noreply.github.com>
2024-02-28 15:32:37 -0800

    Time: 57 ms (55.96%), Space: 51.7 MB (46.65%) - LeetHub

---
+++ 2727-is-object-empty/2727-is-object-empty.ts
@@ -0,0 +1,10 @@
+type JSONValue = null | boolean | number | string | JSONValue[] | { [key: string]: JSONValue };
+type Obj = Record<string, JSONValue> | JSONValue[]
+
+function isEmpty(obj: Obj): boolean {
+    if (Array.isArray(obj)) {
+        return obj.length === 0;
+    } else {
+        return Object.keys(obj).length === 0;
+    }
+}
